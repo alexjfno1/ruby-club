d8cd3d428eb1d9cfff77ab94869738bac039be2c
Alex <Alex>
2013-11-24 21:16:39 +0000

    Fixing and adding tests to the password store app

--- password_store/tests/unit_tests/password_store_tests.rb
+++ password_store/tests/unit_tests/password_store_tests.rb
@@ -1,20 +1,20 @@
 require 'rspec'
-require_relative 'password_store'
+require_relative '../../password_store'
 
 describe PasswordStore, 'PasswordStore' do
 	it 'returns my Amazon password' do
-		expect(PasswordStore.get('amazon')).ito.be.equal('amazonPassword')
+		expect(PasswordStore.get('amazon')).to eq('amazonPassword')
 	end
 
-	it 'return my eBay password' do
-
+	it 'returns my eBay password' do
+		expect(PasswordStore.get('ebay')).to eq('ebayPassword')
 	end
 
 	it 'returns a friendly error message when no a requested account does not exist' do
-
+		expect(PasswordStore.get('nonExistant')).to eq('Sorry but this account doesn\'t exist!')
 	end
 
 	it 'returns a friendly error message when nothing is entered' do
-
+		expect(PasswordStore.get('')).to eq('Please enter an account name!')
 	end
 end
